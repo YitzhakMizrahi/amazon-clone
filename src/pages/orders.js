99c617b4f12c1cb0aaa591db81ac23a290773a22
ymizrahi <yitzhak.tech@gmail.com>
2021-05-30 14:03:45 +0300

    bug fix

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,5 +1,5 @@
-import { getSession, useSession } from 'next-auth/client';
 import moment from 'moment';
+import { getSession, useSession } from 'next-auth/client';
 import db from '../../firebase';
 import Header from '../components/Header';
 import Order from '../components/Order';
@@ -7,17 +7,16 @@ import Order from '../components/Order';
 function Orders({ orders }) {
   const [session] = useSession();
 
-  console.log(orders);
   return (
     <div>
       <Header />
+
       <main className="max-w-screen-lg mx-auto p-10">
         <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">
           Your Orders
         </h1>
-
         {session ? (
-          <h2>{orders.length} Order(s)</h2>
+          <h2>{orders?.length} orders</h2>
         ) : (
           <h2>Please sign in to see your orders</h2>
         )}
@@ -44,38 +43,33 @@ function Orders({ orders }) {
 
 export default Orders;
 
-export const getServerSideProps = async (ctx) => {
+export async function getServerSideProps(context) {
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
   // Get Logged in user session credentials
-  const session = await getSession(ctx);
+  const session = await getSession(context);
 
-  if (!session) {
+  if (!session)
     return {
       props: {},
     };
-  }
 
-  //   Firebase db
-  const stripedOrders = await db
+  const stripeOrders = await db
     .collection('users')
     .doc(session.user.email)
     .collection('orders')
     .orderBy('timestamp', 'desc')
     .get();
 
-  // Stripe orders
   const orders = await Promise.all(
-    stripedOrders.docs.map(async (order) => ({
+    stripeOrders.docs.map(async (order) => ({
       id: order.id,
       amount: order.data().amount,
       amountShipping: order.data().amount_shipping,
       images: order.data().images,
       timestamp: moment(order.data().timestamp.toDate()).unix(),
       items: (
-        await stripe.checkout.sessions.listLineItems(order.id, {
-          limit: 100,
-        })
+        await stripe.checkout.sessions.listLineItems(order.id, { limit: 100 })
       ).data,
     }))
   );
@@ -85,4 +79,4 @@ export const getServerSideProps = async (ctx) => {
       orders,
     },
   };
-};
+}
